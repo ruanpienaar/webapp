767afb1eabac33fc933790cb55ac4f91c9dffabc
ruanpienaar <ruan800@gmail.com>
2019-01-26 22:14:18 +0000

    Dockerfile, ci update

--- apps/webapp/test/webapp_tests.erl
+++ apps/webapp/test/webapp_tests.erl
@@ -1,7 +1,7 @@
--module(_tests).
+-module(webapp_tests).
 -include_lib("eunit/include/eunit.hrl").
 
-_unit_test_() ->
+webapp_http_unit_test_() ->
     {setup,
      % Setup Fixture
      fun() -> 
@@ -14,12 +14,12 @@ _unit_test_() ->
      % List of tests
      [
        % Example test
-       {":func1/0",
+       {"webapp_http:func1/0",
             ?_assert(unit_testing:try_test_fun(fun func1/0))}
      ]
     }.
 
 func1() ->
     ?assert(
-        is_list(:module_info())
+        is_list(webapp_http:module_info())
     ).
